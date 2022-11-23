40b5bf862c2564b644f358a7865f0a2e4c4895e3
ziyadovamid <114071715+ziyadovamid@users.noreply.github.com>
2022-11-23 03:06:28 -0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Button from './components/Button'
+
+
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <><Button/></>
+
+)
